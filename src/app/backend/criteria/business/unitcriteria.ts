c52b2c866377d488943923c4d5380c87d60a87bc
Mehdi BOUZIDI <mehdi.bouzidi.92@gmail.com>
2024-06-11 01:13:56 +0200

    [BUSINESS] CommonCriteria-UnitCriteria

---
+++ src/app/backend/criteria/business/unitcriteria.ts
@@ -0,0 +1,7 @@
+import { CommonCriteria } from "../commoncriteria";
+
+export class UnitCriteria extends CommonCriteria{
+    id: number;
+    code: string;
+    name: string;
+}
---
+++ src/app/backend/criteria/commoncriteria.ts
@@ -0,0 +1,6 @@
+export class CommonCriteria {
+    pages: number;
+    size: number;
+    sort: string;
+    sortColumn: string;
+}
